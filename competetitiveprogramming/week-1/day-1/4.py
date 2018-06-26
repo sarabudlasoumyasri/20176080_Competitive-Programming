01b23190d07a874a469aec7d4e154a3128ea61f8
sarabudlasoumyasri <31233263+sarabudlasoumyasri@users.noreply.github.com>
2018-06-26 18:40:31 +0530

    Add files via upload

---
+++ competetitiveprogramming/week-1/day-1/4.py
@@ -0,0 +1,65 @@
+import unittest
+
+
+def merge_ranges(intervals):
+
+    s = sorted(intervals, key=lambda tup: t[0])
+    merged = []
+    for t in s:
+		if not merged:
+			merged.append(t)
+		else:
+			a = merged.pop()
+            if a[1] >= t[0]:
+                if a[1]>=t[1]:
+                    new_tup = tuple([a[0], a[1]])
+                    merged.append(new_tup)
+                else:
+				    new_tup = tuple([a[0], t[1]])
+				    merged.append(new_tup)
+			else:
+				merged.append(a)
+				merged.append(t)
+    return merged
+    
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_meetings_overlap(self):
+        actual = merge_ranges([(1, 3), (2, 4)])
+        expected = [(1, 4)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_touch(self):
+        actual = merge_ranges([(5, 6), (6, 8)])
+        expected = [(5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meeting_contains_other_meeting(self):
+        actual = merge_ranges([(1, 8), (2, 5)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_stay_separate(self):
+        actual = merge_ranges([(1, 3), (4, 8)])
+        expected = [(1, 3), (4, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_multiple_merged_meetings(self):
+        actual = merge_ranges([(1, 4), (2, 5), (5, 8)])
+        expected = [(1, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_meetings_not_sorted(self):
+        actual = merge_ranges([(5, 8), (1, 4), (6, 8)])
+        expected = [(1, 4), (5, 8)]
+        self.assertEqual(actual, expected)
+
+    def test_sample_input(self):
+        actual = merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+        expected = [(0, 1), (3, 8), (9, 12)]
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
