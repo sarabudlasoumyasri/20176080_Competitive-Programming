603b2e19f5aecba0c0ce0944805d3e44d62bd795
sarabudlasoumyasri <31233263+sarabudlasoumyasri@users.noreply.github.com>
2018-06-27 17:16:02 +0530

    Update blanced tree.java

--- competetitiveprogramming/week-1/Day-3/blanced tree.java
+++ competetitiveprogramming/week-1/Day-3/blanced tree.java
@@ -1,59 +1,167 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Stack;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-private static class NodeDepthPair {
+import static org.junit.Assert.*;
 
-    BinaryTreeNode node;
-    int depth;
+public class Solution {
 
-    NodeDepthPair(BinaryTreeNode node, int depth) {
-        this.node = node;
-        this.depth = depth;
+    public static class BinaryTreeNode {
+
+        public int value;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue);
+            return this.left;
+        }
+
+        public BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue);
+            return this.right;
+        }
     }
-}
+    
+    private static class NodeDepthPair {
+    
+        BinaryTreeNode node;
+        int depth;
+    
+        NodeDepthPair(BinaryTreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+     }
 
-public static boolean isBalanced(BinaryTreeNode treeRoot) {
+    public static boolean isBalanced(BinaryTreeNode treeRoot) {
 
-    if (treeRoot == null) {
+        if (treeRoot == null) 
+            return true;
+    
+        ArrayList<Integer> depths = new ArrayList<Integer>();
+    
+        Stack<NodeDepthPair> nodes = new Stack<NodeDepthPair>();
+        
+        nodes.push(new NodeDepthPair(treeRoot, 0));
+    
+        while (!nodes.empty()) {
+    
+            NodeDepthPair nodeDepthPair = nodes.pop();
+            BinaryTreeNode node = nodeDepthPair.node;
+            int depth = nodeDepthPair.depth;
+    
+            if (node.left == null && node.right == null) {
+    
+                if (!depths.contains(depth)) {
+                    depths.add(depth);
+                    
+                    if (depths.size() > 2 || (depths.size() == 2
+                            && Math.abs(depths.get(0) - depths.get(1)) > 1)) {
+                        return false;
+                    }
+                }
+    
+            } 
+            
+            else {
+                if (node.left != null) {
+                    nodes.push(new NodeDepthPair(node.left, depth + 1));
+                }
+                if (node.right != null) {
+                    nodes.push(new NodeDepthPair(node.right, depth + 1));
+                }
+            }
+        }
+    
         return true;
     }
 
-    List<Integer> depths = new ArrayList<>(3);
 
 
-    Stack<NodeDepthPair> nodes = new Stack<>();
-    nodes.push(new NodeDepthPair(treeRoot, 0));
+    // tests
 
-    while (!nodes.empty()) {
-
-       
-        NodeDepthPair nodeDepthPair = nodes.pop();
-        BinaryTreeNode node = nodeDepthPair.node;
-        int depth = nodeDepthPair.depth;
-
-        if (node.left == null && node.right == null) {
-
-            if (!depths.contains(depth)) {
-                depths.add(depth);
-
-                
-                if (depths.size() > 2 || (depths.size() == 2
-                        && Math.abs(depths.get(0) - depths.get(1)) > 1)) {
-                    return false;
-                }
-            }
-
-      
-        } else {
-            if (node.left != null) {
-                nodes.push(new NodeDepthPair(node.left, depth + 1));
-            }
-            if (node.right != null) {
-                nodes.push(new NodeDepthPair(node.right, depth + 1));
-            }
-        }
+    @Test
+    public void fullTreeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(5);
+        final BinaryTreeNode a = root.insertLeft(8);
+        final BinaryTreeNode b = root.insertRight(6);
+        a.insertLeft(1);
+        a.insertRight(2);
+        b.insertLeft(3);
+        b.insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
     }
 
-    return true;
+    @Test
+    public void bothLeavesAtTheSameDepthTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(3);
+        root.insertLeft(4).insertLeft(1);
+        root.insertRight(2).insertRight(9);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByOneTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByTwoTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7).insertRight(8);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void bothSubTreesSuperbalancedTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertLeft(5);
+        final BinaryTreeNode b = root.insertRight(9);
+        b.insertLeft(8).insertLeft(7);
+        b.insertRight(5);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void onlyOneNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void treeIsLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertRight(2).insertRight(3).insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
 }
