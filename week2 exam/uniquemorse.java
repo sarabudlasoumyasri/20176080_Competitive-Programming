5c0c3af74da4c54deca22ae8c82e9c8dee081cc0
sarabudlasoumyasri <31233263+sarabudlasoumyasri@users.noreply.github.com>
2018-07-12 15:23:39 +0530

    Update uniquemorse.java

--- week2 exam/uniquemorse.java
+++ week2 exam/uniquemorse.java
@@ -1,1 +1,36 @@
 
+import java.util.*;
+class morse{
+
+	public static ArrayList<String> obj=new ArrayList<String>();
+
+	public static String uniquemorse(String arr[],String k){
+		String a="";
+		for (int i=0;i<k.length();i++) {
+			int m=k.charAt(i)-97;
+			a+=arr[m];
+		}
+		if(!obj.contains(a))
+			obj.add(a);
+		return a;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String[] array = {".-","-...","-.-.","-..",".","..-.","--.",
+                         "....","..",".---","-.-",".-..","--","-.",
+                         "---",".--.","--.-",".-.","...","-","..-",
+                         "...-",".--","-..-","-.--","--.."};
+		String org[]={"gin", "zen", "gig","msg"};
+	
+
+			for (int j=0;j<org.length ;j++) {
+					System.out.println(org[j]+"-------->"+uniquemorse(array,org[j]));	
+			}
+
+		System.out.println("total transformations = "+obj.size());
+
+	}
+}
+
+
